aa64dd7ff6a0d43bc2c8a37fb2b3e360136f333a
Loginov Vladimir <suigintounotsuikaima@gmail.com>
2025-11-25 23:29:17 +0900

    fix sms sql

--- SerialPort/sms.cpp
+++ SerialPort/sms.cpp
@@ -90,7 +90,7 @@ void Sms::saveToDB()
     std::string query="INSERT INTO sms(isReceived, datetime, number, msg, isReaded)"
                         "VALUES(";
     query+=received_;
-    query+=",\""+datetime_+"\",\""+number_+"\",\""+message_+"\",0)";
+    query+=",\""+datetime_+"\",\""+number_+"\",\""+message_+"\",0);";
     dbInstance->executeQuery(query.c_str());
 
 }
